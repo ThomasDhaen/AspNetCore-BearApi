f4e66bd371000cef7e9049eee8b535f0bfca9606
Thomas D'haen <thomasdhaen@me.com>
2018-01-28 18:28:33 +0100

    Add Interface DBRepo

---
+++ BlackBearsApi/Repositories/IDbCollectionRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlackBearsApi.Repositories
+{
+    public interface IDbCollectionRepository<TModel, in TParam>
+    {
+        Task<IEnumerable<TModel>> GetItemsFromCollectionAsync();
+        Task<TModel> GetItemFromCollectionAsync(TParam id);
+        Task<TModel> AddDocumentIntoCollectionAsync(TModel item);
+        Task<TModel> UpdateDocumentFromCollection(TParam id, TModel item);
+        Task DeleteDocumentFromCollectionAsync(TParam id);
+    }
+}
