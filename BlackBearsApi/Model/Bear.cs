b665822ec8cf0e8c3850da72055621439ed4a356
Thomas D'haen <thomasdhaen@me.com>
2018-01-28 18:23:51 +0100

    Add JsonProperties to Models

--- BlackBearsApi/Model/Bear.cs
+++ BlackBearsApi/Model/Bear.cs
@@ -1,8 +1,13 @@
-﻿namespace WebApplication1.Model
+﻿using Newtonsoft.Json;
+
+namespace WebApplication1.Model
 {
     public class Bear
     {
+        [JsonProperty(PropertyName = "name")]
         public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "weight")]
         public int Weight { get; set; }
     }
 }
--- BlackBearsApi/Model/Food.cs
+++ BlackBearsApi/Model/Food.cs
@@ -1,8 +1,13 @@
-﻿namespace WebApplication1.Model
+﻿using Newtonsoft.Json;
+
+namespace WebApplication1.Model
 {
     public class Food
     {
+        [JsonProperty(PropertyName = "name")]
         public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "kCal")]
         public int KCal { get; set; }
     }
 }
